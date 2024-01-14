fcb47abb71d4021418c3ab04ab824c46357772ec
Desh Deepak Singh <Desh.Deepak.Singh@ibm.com>
2024-01-14 15:38:35 +0530

    Task 2

--- app.js
+++ app.js
@@ -7,17 +7,29 @@ stars.forEach(star => {
     })
 })
 
-const box = document.getElementById("box");
-box.addEventListener("click", () => {
-    box.className = '' === box.className ? '' : 'toggled';
-    box.style.backgroundColor = "red" === box.style.backgroundColor ? "red" : "blue"
-    // if (box.classList == ''){
-    //     box.style.backgroundColor = "blue"
-    //     box.className = "toggled"
-    // }
-    // else {
-    //     box.style.backgroundColor = "red"
-    //     box.className = ''
-    // }
+// Task 2
+
+const popover = document.getElementById('popover');
+const popoverContent = document.getElementById('popover-content');
+popoverContent.style.color = "white";
+popover.addEventListener("mouseenter", () => {
+    popoverContent.style.display = "block";
 })
+popover.addEventListener("mouseleave", () => {
+    popoverContent.style.display = "none";
+});
+
+// const box = document.getElementById("box");
+// box.addEventListener("click", () => {
+//     box.className = '' === box.className ? '' : 'toggled';
+//     box.style.backgroundColor = "red" === box.style.backgroundColor ? "red" : "blue"
+//     // if (box.classList == ''){
+//     //     box.style.backgroundColor = "blue"
+//     //     box.className = "toggled"
+//     // }
+//     // else {
+//     //     box.style.backgroundColor = "red"
+//     //     box.className = ''
+//     // }
+// })
 
